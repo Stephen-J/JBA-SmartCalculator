3b5701452d7a129af7b35f83a4f5ce8801ee38d1
Stephen Johnson <steve.eric.johnson@gmail.com>
2021-01-31 14:43:27 -0700

    Stage 1/8

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,5 +1,8 @@
 package calculator
 
+
 fun main() {
-    println("Hello, World!")
+    val input = readLine()!!.split(" ").map {it.toInt()}
+    val result = input.fold(0){sum,num -> sum + num}
+    println(result)
 }
