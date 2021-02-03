35093dfc7ee0a46ffee9ba2c87bfe114e0390762
Stephen Johnson <steve.eric.johnson@gmail.com>
2021-02-02 22:05:03 -0700

    stage 3/8

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -5,10 +5,13 @@ fun main() {
     do {
         val input = readLine()!!.split(" ")
         when {
-            input.size == 2 -> println(input[0].toInt() + input[1].toInt())
-            input.size == 1 && input[0] == "/exit" -> println("Bye!")
-            input.size == 1 && input[0] != "/exit" && input[0] != "" -> println(input[0])
-
+            input.size >= 2 -> println(input.fold(0){ accum,v -> accum + v.toInt() })
+            input.size == 1 -> when (input[0]) {
+                "/exit" -> println("Bye!")
+                "/help" -> println("The program calculates the sum of numbers")
+                "" -> Unit
+                else -> println(input[0])
+            }
         }
     } while (input[0] != "/exit")
 }
